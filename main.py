54914b67cf66ae8a99894f4daa8417ffd2e68307
Zhe Zhang <nakayamaqs@hotmail.com>
2015-07-07 13:39:27 +0800

    update main func

--- main.py
+++ main.py
@@ -6,18 +6,10 @@ __author__ = 'Zhe'
 from yunsoo.sheet_consumer import *
 import datetime
 
-
-class Hello:
-    def __init__(self):
-        pass
-
-    def sayHello(self):
-        print("This is Hello! -> by " + self)
-
-# work_sheet_id = ['4.S.19739162398753', '4.S.19740602643438', '4.S.19738931498116','4.S.19739337276415']
-# company_unit_id = ['4.SA.组织ID.19739176367353','4.SA.组织ID.19740604775335','4.SA.组织ID.19738938410218','4.SA.组织ID.19739861318573']
 org_id_list = ['2k0r0963j0akld83lsd2', '2k0r1l55i2rs5544wz5', '2k0r2yvydbxbvibvgfm', '2k0r306o609oljxd1hh',
                '2kbyyjauwtate9syvmy']
+
+# org_id_list = ['2kbyyjauwtate9syvmy']
 
 work_sheet_input = {
     '4.S.19739162398753': {
